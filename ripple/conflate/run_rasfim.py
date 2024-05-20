3a0a2103964d423d8165675aef743b58591db017
Seth Lawler <slawler@dewberry.com>
2024-05-20 09:05:41 -0400

    replace run with run_rasfim

--- ripple/conflate/run_rasfim.py
+++ ripple/conflate/run_rasfim.py
@@ -1,69 +1,67 @@
-import json
-import boto3
-import geopandas as gpd
-import os
-from pathlib import Path
-
-# from .rasfim import RasFimConflater
-from rasfim import RasFimConflater
-from rasfim import (
+from .rasfim import RasFimConflater
+from .rasfim import (
     convert_linestring_to_points,
-    compare_nearest_points,
-    count_intersecting_lines,
+    calculate_conflation_metrics,
+    nearest_line_to_point,
+    conflation_summary,
+    find_ds_most_branch,
+    walk_branches,
+    map_control_nodes_to_xs,
+    find_flow_change_locations,
 )
 
 
-import dotenv
+def point_method_conflation(rfc: RasFimConflater, river_reach_name: str) -> dict:
+    """
+    Conflate a single river reach with nwm branches
+    Method 1: Nearest Points
+    """
 
-dotenv.load_dotenv()
-
-bucket = "fim"
-nwm_gpkg = f"/vsis3/fim/mip/dev/branches.gpkg"
-ras_gpkg = "/vsis3/fim/mip/dev/Crystal Creek-West Fork San Jacinto River/STEWARTS CREEK/STEWARTS CREEK.gpkg"
-
-rfc = RasFimConflater(nwm_gpkg, ras_gpkg)
-
-rfc.load_data()
-
-# rfc.ras_river_reach_names
-
-for river_reach_name in rfc.ras_river_reach_names:
     xs_group = rfc.xs_by_river_reach_name(river_reach_name)
 
-
-for river_reach_name in rfc.ras_river_reach_names:
-    buffered_ras_centerline_points = rfc.ras_centerline_densified_points(
+    # Buffer the ras centerline points to improve intersection with nwm branches
+    ras_centerline_to_densified_points = rfc.ras_centerline_densified_points(
         river_reach_name
     )
 
-candidate_branches = rfc.candidate_nwm_branches(buffered_ras_centerline_points, 10)
-
-ras_points = convert_linestring_to_points(rfc.ras_centerlines.loc[0].geometry)
-
-
-next_round_candidates = []
-total_hits = 0
-for i in candidate_branches.index:
-    candidate_branch_points = convert_linestring_to_points(
-        candidate_branches.loc[i].geometry
+    # Subset the branches to only those that are within 10 meters of the ras centerline points
+    # This could use nearest instead of the buffer/interesct approach,
+    candidate_branches = rfc.candidate_nwm_branches_via_point_buffer(
+        ras_centerline_to_densified_points, 10
     )
-    if compare_nearest_points(candidate_branch_points, ras_points) < 2000:
-        next_round_candidates.append(candidate_branches.loc[i]["branch_id"])
-        gdftmp = gpd.GeoDataFrame(
-            geometry=[candidate_branches.loc[i].geometry], crs=rfc.nwm_branches.crs
-        )
-        xs_hits = count_intersecting_lines(xs_group, gdftmp)
-        total_hits += xs_hits
 
-        print(total_hits, xs_group.shape[0])
+    # Convert the ras centerline to points
+    ras_points = convert_linestring_to_points(
+        rfc.ras_centerlines.loc[0].geometry, crs=rfc.common_crs, point_spacing=10
+    )
 
-conflation_score = total_hits / xs_group.shape[0]
+    # fim_stream = rfc.nwm_branches[rfc.nwm_branches["branch_id"].isin(candidate_branches)]
+    # plot_conflation_results(rfc, candidate_branches, "test.png")
 
-if conflation_score == 1:
-    print("Conflation complete")
-elif conflation_score >= 0.25:
-    print(f"Check: disconnected branches score: {conflation_score}")
-elif conflation_score < 0.25:
-    print(f"Fail: disconnected branches score: {conflation_score}")
-elif conflation_score > 1:
-    print(f"Fail: diverging branches score: {conflation_score}")
+    # Find the cross sections that intersect the ras centerline and calculate conflation metrics
+    return calculate_conflation_metrics(
+        rfc,
+        candidate_branches,
+        xs_group,
+        ras_points,
+    )
+
+
+def conflate_branches(rfc: RasFimConflater) -> dict:
+    ras_start_point, ras_stop_point = rfc.ras_start_end_points()
+
+    candidate_reaches = rfc.intersected_ras_river_nwm_branches()
+
+    us_most_branch_id = nearest_line_to_point(candidate_reaches, ras_start_point)
+
+    ds_most_branch_id = find_ds_most_branch(rfc, rfc.nwm_nodes, ras_stop_point)
+
+    branch_info = walk_branches(rfc, us_most_branch_id, ds_most_branch_id)
+
+    branch_info_with_ras_xs = map_control_nodes_to_xs(rfc, branch_info)
+
+    flow_changes = find_flow_change_locations(branch_info_with_ras_xs)
+
+    summary = conflation_summary(rfc, branch_info_with_ras_xs, flow_changes)
+
+    return us_most_branch_id, ds_most_branch_id, summary
