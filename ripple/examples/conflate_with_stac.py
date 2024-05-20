cc7001f42976591ca3e4d55b1ad6cbf681e41de4
Seth Lawler <slawler@dewberry.com>
2024-05-20 09:05:59 -0400

    update example conflation script

--- ripple/examples/conflate_with_stac.py
+++ ripple/examples/conflate_with_stac.py
@@ -1,71 +1,90 @@
+from datetime import datetime
 import pystac_client
 import pystac
 import os
 import boto3
 import json
 import requests
-from urljoin import url_path_join
 import logging
 
-from pathlib import Path    
+from pathlib import Path
 
-from ripple.conflate.ras1d import RasFimConflater, nwm_conflated_reaches
-from ripple.conflate import run
+from ripple.conflate.rasfim import RasFimConflater, nwm_conflated_reaches
+from ripple.conflate.rasfim import STAC_API_URL
+from ripple.conflate.run_rasfim import conflate_branches, point_method_conflation
 from ripple.conflate.plotter import plot_conflation_results
 
-from ripple.conflate.ras1d import STAC_API_URL
+logging.getLogger("fiona").setLevel(logging.ERROR)
+logging.getLogger("botocore").setLevel(logging.ERROR)
 
 
-def create_update_item(endpoint: str, collection_id: str, item: pystac.Item):
-    items_url = url_path_join(endpoint, f"collections/{collection_id}/items")
+def upsert_item(endpoint: str, collection_id: str, item: pystac.Item):
+    items_url = f"{endpoint}/collections/{collection_id}/items"
     response = requests.post(items_url, json=item.to_dict())
     if response.status_code == 409:
-        item_update_url = url_path_join(items_url, f"{item.id}")
+        item_update_url = f"{items_url}/{item.id}"
         response = requests.put(item_update_url, json=item.to_dict())
+    if not response.ok:
+        return f"Response from STAC API: {response.status_code}"
 
-def href_to_vsis(href:str, bucket:str) -> str:
-    return href.replace(f"https://{bucket}.s3.amazonaws.com",f"/vsis3/{bucket}")
+
+def href_to_vsis(href: str, bucket: str) -> str:
+    return href.replace(f"https://{bucket}.s3.amazonaws.com", f"/vsis3/{bucket}")
 
 
-def main(item, nwm_gpkg, client, bucket, collection_id):
-    for asset in item.get_assets(role="ras-geometry-gpkg"):
-        gpkg_name = Path(item.assets[asset].href).name 
-        s3_prefix =item.assets[asset].href.replace(f"https://{bucket}.s3.amazonaws.com/","").replace(f"/{gpkg_name}","")
-        ras_gpkg =href_to_vsis(item.assets[asset].href, bucket="fim")
+def main(item, client, bucket, collection_id, rfc, river_reach_name):
+    conflation_metrics = point_method_conflation(rfc, river_reach_name)
 
-    rfc = RasFimConflater(nwm_gpkg, ras_gpkg)
-    us_most_branch_id, ds_most_branch_id, summary = run.main(rfc)
+    if conflation_metrics["conflation_score"] == 0:
+        fim_stream = rfc.nwm_branches
+        limit_plot = False
+    else:
+        us_most_branch_id, ds_most_branch_id, summary = conflate_branches(rfc)
 
-    item.properties["NWM_FIM:Upstream_Branch_ID"] = us_most_branch_id
-    item.properties["NWM_FIM:Downstream_Branch_ID"] = ds_most_branch_id
+        item.properties["NWM_FIM:Upstream_Branch_ID"] = us_most_branch_id
+        item.properties["NWM_FIM:Downstream_Branch_ID"] = ds_most_branch_id
 
-    fim_stream = nwm_conflated_reaches(rfc, summary)
+        fim_stream = nwm_conflated_reaches(rfc, summary)
 
-    ripple_parameters_key = f"{s3_prefix}/ripple_parameters.json"
-    ripple_parameters_href = f"https://{bucket}.s3.amazonaws.com/{ripple_parameters_key}"
+        ripple_parameters_key = f"{s3_prefix}/ripple_parameters.json"
+        ripple_parameters_href = (
+            f"https://{bucket}.s3.amazonaws.com/{ripple_parameters_key}"
+        )
 
-    client.put_object(
-        Body=json.dumps(summary).encode(),
-        Bucket=bucket,
-        Key=ripple_parameters_key,
+        client.put_object(
+            Body=json.dumps(summary).encode(),
+            Bucket=bucket,
+            Key=ripple_parameters_key,
+        )
+
+        # Add ripple parameters asset to item
+        item.add_asset(
+            ripple_parameters_key.replace(" ", ""),
+            pystac.Asset(
+                ripple_parameters_href,
+                title="ConflationParameters",
+                roles=[pystac.MediaType.JSON, "ripple-params"],
+                extra_fields={
+                    "software": "ripple v0.1.0-alpha.1",
+                    "date_created": datetime.now().isoformat(),
+                },
+            ),
+        )
+
+        for asset in item.get_assets():
+            item.assets[asset].href = item.assets[asset].href.replace(
+                "https:/fim", "https://fim"
+            )
+        limit_plot = True
+
+    conflation_thumbnail_key = (
+        f"stac/{collection_id}/thumbnails/{item.id}-conflation.png".replace(" ", "")
+    )
+    conflation_thumbnail_href = (
+        f"https://{bucket}.s3.amazonaws.com/{conflation_thumbnail_key}"
     )
 
-    # Add ripple parameters asset to item
-    item.add_asset(
-        ripple_parameters_key.replace(" ",""),
-        pystac.Asset(
-            ripple_parameters_href,
-            title="ConflationParameters",
-            roles=[pystac.MediaType.JSON, "ripple-params"],
-           extra_fields={"software": "ripple v0.1.0-alpha.1", "date_created": "5-1-2024"},
-        ),
-    )
-
-    for asset in item.get_assets():
-        item.assets[asset].href = item.assets[asset].href.replace("https:/fim","https://fim")
-
-    conflation_thumbnail_key = f"stac/{collection_id}/thumbnails/{item.id}-conflation.png"
-    conflation_thumbnail_href = f"https://{bucket}.s3.amazonaws.com/{conflation_thumbnail_key}"
+    item.properties["NWM_FIM:Conflation_Metrics"] = conflation_metrics
 
     plot_conflation_results(
         rfc,
@@ -73,57 +92,71 @@ def main(item, nwm_gpkg, client, bucket, collection_id):
         conflation_thumbnail_key,
         bucket=bucket,
         s3_client=client,
+        limit_plot_to_nearby_branches=limit_plot,
     )
 
     # Add thumbnal asset to item
     item.add_asset(
         conflation_thumbnail_key,
         pystac.Asset(
-            conflation_thumbnail_href.replace(" ",""),
-            title="ConflationThumbnail",
-            roles=[pystac.MediaType.PNG, "Thumbnail"],
-           extra_fields={"software": "ripple v0.1.0-alpha.1", "date_created": "5-1-2024"},
+            conflation_thumbnail_href.replace(" ", ""),
+            title="ThumbnailConflationResults",
+            roles=[pystac.MediaType.PNG, "thumbnail"],
+            extra_fields={
+                "software": "ripple v0.1.0-alpha.1",
+                "date_created": datetime.now().isoformat(),
+            },
         ),
     )
 
-    create_update_item(STAC_API_URL, collection_id, item)
+    upsert_item(STAC_API_URL, collection_id, item)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
-    logging.basicConfig(level=logging.ERROR,
-                        filename="conflate_with_stac-v3.log")
+    logging.basicConfig(level=logging.INFO, filename="conflate_with_stac-v1.log")
 
-    STAC_API_URL = "https://stac.dewberryanalytics.com"
     collection_id = "huc-12040101"
+    bucket = "fim"
 
     # Connect to STAC API
     client = pystac_client.Client.open(STAC_API_URL)
     collection = client.get_collection(collection_id)
-    branch_item = collection.get_item("nwm-huc-12040101-branches-v2024-5-4")
 
     # Fetch the branch geopackage containint control points
-    for asset in branch_item.get_assets(role="ras-conflation"):
-        nwm_gpkg = href_to_vsis(branch_item.assets[asset].href, bucket="fim")
-        
+    # role="ras-conflation"
+    # for asset in collection.get_assets():
+
+    branches_s3_key = collection.assets["conflation-ref-v1"].extra_fields["s3_key"]
+    nwm_gpkg = f"/vsis3/{branches_s3_key}"
+    # print("nwm_gpkg", branches_s3_key)
+
     session = boto3.Session(
         aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
     )
 
     client = session.client("s3")
-    bucket = "fim"
 
     # Get all items in the collection
     items = collection.get_all_items()
     for item in items:
-        print(item.id)
-        if item.id == branch_item.id:
-            continue
+        for asset in item.get_assets(role="ras-geometry-gpkg"):
+            gpkg_name = Path(item.assets[asset].href).name
+            s3_prefix = (
+                item.assets[asset]
+                .href.replace(f"https://{bucket}.s3.amazonaws.com/", "")
+                .replace(f"/{gpkg_name}", "")
+            )
+            ras_gpkg = href_to_vsis(item.assets[asset].href, bucket="fim")
+
+        rfc = RasFimConflater(nwm_gpkg, ras_gpkg)
+
+        for river_reach_name in rfc.ras_river_reach_names:
+            print(item.id, river_reach_name)
 
         try:
-            main(item, nwm_gpkg, client, bucket, collection_id)
-            logging.critical(f"{item.id}: Successfully processed")
+            main(item, client, bucket, collection_id, rfc, river_reach_name)
+            logging.info(f"{item.id}: Successfully processed")
         except Exception as e:
             logging.error(f"{item.id}: Error processing | {e}")
-            continue
