94fac12afeab7a31f862854866e1b613f23e8fcd
Matt Deshotel <mdeshotel@dewberry.com>
2024-06-15 11:20:35 -0500

    subset conflation parameters

--- ripple/scripts/subset_gpkg.py
+++ ripple/scripts/subset_gpkg.py
@@ -4,37 +4,52 @@ import os
 from ripple.process import subset_gpkg
 
 
-def main(ras_project_directory: str, ras_gpkg_file_path: str, nwm_id: str, conflation_parameters: dict):
+def main(ras_project_directory: str, ras_gpkg_file_path: str, nwm_id: str, ripple_parameters: dict, ripple_version):
 
-    if conflation_parameters["us_xs"]["xs_id"] == "-9999":
-        print(f"skipping {nwm_id}; no cross sections conflated.")
+    if ripple_parameters["us_xs"]["xs_id"] == "-9999":
+        ripple_parameters["messages"] = f"skipping {nwm_id}; no cross sections conflated."
+        print(ripple_parameters["messages"])
     else:
-        subset_gpkg_path = subset_gpkg(
+        subset_gpkg_path, crs = subset_gpkg(
             ras_gpkg_file_path,
             ras_project_directory,
             nwm_id,
-            conflation_parameters["ds_xs"]["xs_id"],
-            conflation_parameters["us_xs"]["xs_id"],
-            conflation_parameters["us_xs"]["river"],
-            conflation_parameters["us_xs"]["reach"],
-            conflation_parameters["ds_xs"]["river"],
-            conflation_parameters["ds_xs"]["reach"],
+            ripple_parameters["ds_xs"]["xs_id"],
+            ripple_parameters["us_xs"]["xs_id"],
+            ripple_parameters["us_xs"]["river"],
+            ripple_parameters["us_xs"]["reach"],
+            ripple_parameters["ds_xs"]["river"],
+            ripple_parameters["ds_xs"]["reach"],
         )
+
+        ripple_parameters["files"] = {"gpkg": subset_gpkg_path}
+        ripple_parameters["crs"] = crs
+        ripple_parameters["version"] = ripple_version
+
+        with open(os.path.join(ras_project_directory, f"{nwm_id}.ripple.json"), "w") as f:
+            json.dump({nwm_id: ripple_parameters}, f, indent=4)
+
+    return ripple_parameters
 
 
 if __name__ == "__main__":
 
-    ras_project_directory = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\tests\ras-data\WFSJMAIN\nwm_models"
-    ras_gpkg_file_path = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\tests\ras-data\WFSJMain\WFSJMAIN.gpkg"
+    ras_project_directory = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\tests\ras-data\WFSJMain\nwm_models"
+    ras_gpkg_file_path = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\tests\ras-data\WFSJMain\WFSJMain.gpkg"
     conflation_json_path = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\tests\ras-data\WFSJMain\WFSJ Main.json"
-
+    ripple_version = "0.0.1"
     with open(conflation_json_path) as f:
         conflation_parameters = json.load(f)
 
+    ripple_parameters = {}
     for nwm_id in conflation_parameters.keys():
         print(f"working on {nwm_id}")
-        main(os.path.join(ras_project_directory, nwm_id), ras_gpkg_file_path, nwm_id, conflation_parameters[nwm_id])
-
-import os
-
-"1468322" in os.listdir(r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\tests\ras-data\WFSJMain\nwm_models")
+        ripple_parameters[nwm_id] = main(
+            os.path.join(ras_project_directory, nwm_id),
+            ras_gpkg_file_path,
+            nwm_id,
+            conflation_parameters[nwm_id],
+            ripple_version,
+        )
+    with open(conflation_json_path, "w") as f:
+        json.dump(ripple_parameters, f, indent=4)
