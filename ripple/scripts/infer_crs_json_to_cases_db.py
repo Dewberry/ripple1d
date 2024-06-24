2f0214847bbc3d43c0d1f8f77e884ad54486e455
Matt Deshotel <mdeshotel@dewberry.com>
2024-06-24 15:13:01 -0500

    handle single and mulitple potential crs with new infer_crs_json format

--- ripple/scripts/infer_crs_json_to_cases_db.py
+++ ripple/scripts/infer_crs_json_to_cases_db.py
@@ -10,12 +10,33 @@ def read_inferred_crs_json(inferred_crs_json_path: str) -> dict:
 
 
 def update_key_base_path(data: list, new_key_base: str, old_key_base: str) -> list:
-    new_data = []
-    for item in data:
-        if item["key"]:
-            item["key"] = posixpath.join(new_key_base, item["key"].lstrip(old_key_base).replace("\\", "/"))
-            new_data.append(item)
+    new_data = {}
+    for key, val in data.items():
+        if key != "null":
+            key = posixpath.join(new_key_base, key.lstrip(old_key_base).replace("\\", "/"))
+            new_data[key] = val
     return new_data
+
+
+def create_table(cases_db_path: str, table_name: str):
+    with sqlite3.connect(cases_db_path) as connection:
+        cursor = connection.cursor()
+
+        # create inferred crs A table if it doesn't exists
+        res = cursor.execute(f"SELECT name FROM sqlite_master WHERE name='{table_name}'")
+        if res.fetchone():
+            cursor.execute(f"DROP TABLE {table_name}")
+        connection.commit()
+
+        sql_query = f"""
+        CREATE TABLE {table_name}(
+        mip_case TEXT,
+        key TEXT,
+        crs TEXT,
+        ratio_of_best_crs TEXT)
+        """
+        cursor.execute(sql_query)
+        connection.commit()
 
 
 def main(inferred_crs_json_path: str, new_key_base: str, old_key_base: str, cases_db_path: str):
@@ -26,36 +47,35 @@ def main(inferred_crs_json_path: str, new_key_base: str, old_key_base: str, case
     if new_key_base and old_key_base:
         crs_data = update_key_base_path(crs_data, new_key_base, old_key_base)
 
+    for table_name in ["inferred_crs_A", "inferred_crs_B"]:
+        create_table(cases_db_path, table_name)
+
     with sqlite3.connect(cases_db_path) as connection:
         cursor = connection.cursor()
 
-        # create inferred crs A table if it doesn't exists
-        res = cursor.execute("SELECT name FROM sqlite_master WHERE name='inferred_crs_A'")
-        if res.fetchone():
-            cursor.execute("DROP TABLE inferred_crs_A")
-        connection.commit()
-
-        sql_query = """
-        CREATE TABLE inferred_crs_A(
-        mip_case TEXT,
-        key TEXT,
-        crs TEXT)
-        """
-        cursor.execute(sql_query)
-        connection.commit()
-        for data in crs_data:
-            if data["crscode2intersectratio"] and len(data["crscode2intersectratio"]) == 1:
-                crs = list(data["crscode2intersectratio"].keys())[0]
+        for key, data in crs_data.items():
+            epsgs = []
+            for county in data["county_results"]:
+                if county["crscode2intersectratio"]:
+                    epsgs += list(county["crscode2intersectratio"].keys())
+                    
+            if data["best_crs"] and len(set(epsgs)) == 1:
                 cursor.execute(
-                    "INSERT INTO inferred_crs_A (mip_case, key, crs) VALUES (?, ?, ?)",
-                    (data["mip_case"], data["key"], crs),
+                    "INSERT INTO inferred_crs_A (mip_case, key, crs, ratio_of_best_crs) VALUES (?, ?, ?, ?)",
+                    (data["county_results"][0]["mip_case"], key, data["best_crs"], data["ratio_of_best_crs"]),
                 )
+            elif data["best_crs"] and len(set(epsgs)) > 1:
+                cursor.execute(
+                    "INSERT INTO inferred_crs_B (mip_case, key, crs, ratio_of_best_crs) VALUES (?, ?, ?, ?)",
+                    (data["county_results"][0]["mip_case"], key, data["best_crs"], data["ratio_of_best_crs"]),
+                )
+
         connection.commit()
 
 
 if __name__ == "__main__":
 
-    inferred_crs_json_path = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\mip_models\ras_projects_crs_inference\ras_projects_crs_inference.json"
+    inferred_crs_json_path = r"C:\Users\mdeshotel\Downloads\ras_projects_crs_inference_v2esque_with_best_crs\ras_projects_crs_inference_v2esque.json"
     new_key_base = r"s3://fim/mip/cases"
     old_key_base = r"T:\CCSI\TECH\owp\mip"
     cases_db_path = r"C:\Users\mdeshotel\Downloads\12040101_Models\ripple\mip_models\cases.db"
