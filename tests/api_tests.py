7d7a87bb5c7484cb7d6b812bfad8cf5bf7742b9b
Matt Deshotel <mdeshotel@dewberry.com>
2024-07-15 08:07:37 -0500

    assert files exists

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -12,6 +12,23 @@ TEST_DIR = os.path.dirname(__file__)
 
 SOURCE_RAS_MODEL_DIRECTORY = os.path.join(TEST_DIR, "ras-data\\Baxter")
 SUBMODELS_BASE_DIRECTORY = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels")
+GPKG_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.gpkg")
+TERRAIN_HDF = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\Terrain\\2823932.hdf")
+TERRAIN_VRT = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\Terrain\\2823932.vrt")
+RAS_PROJECT_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.prj")
+GEOM_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.g01")
+PLAN1_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.p01")
+FLOW1_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.f01")
+RESULT1_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.r01")
+PLAN2_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.p02")
+FLOW2_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.f02")
+RESULT2_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.r02")
+PLAN3_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.p03")
+FLOW3_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.f03")
+RESULT3_FILE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\2823932.r03")
+FIM_LIB_DB = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\fims\\2823932.db")
+DEPTH_GRIDS_ND = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\fims\\z_0_0")
+DEPTH_GRIDS_KWSE = os.path.join(TEST_DIR, "ras-data\\Baxter\\submodels\\2823932\\fims\\z_60_0")
 REACH_ID = "2823932"
 
 
@@ -36,11 +53,13 @@ def wait_for_job(job_id: str):
 def check_process(func):
 
     def wrapper(self, *args, **kwargs):
-        process, payload = func(self)
+        process, payload, files = func(self)
         response = submit_job(process, payload)
         status = wait_for_job(response["jobID"])
 
         self.assertEqual(status, "successful")
+        for file in files:
+            self.assertTrue(os.path.exists(file))
 
     return wrapper
 
@@ -56,14 +75,15 @@ class TestApi(unittest.TestCase):
             "ripple_version": RIPPLE_VERSION,
         }
         process = "extract_submodel"
-
-        return process, payload
+        files = [GPKG_FILE]
+        return process, payload, files
 
     @check_process
     def test2_create_ras_terrain(self):
         payload = {"submodel_directory": f"{SUBMODELS_BASE_DIRECTORY}\\{REACH_ID}"}
         process = "create_ras_terrain"
-        return process, payload
+        files = [TERRAIN_HDF, TERRAIN_VRT]
+        return process, payload, files
 
     @check_process
     def test3_create_model_run_normal_depth(self):
@@ -74,7 +94,8 @@ class TestApi(unittest.TestCase):
             "show_ras": False,
         }
         process = "create_model_run_normal_depth"
-        return process, payload
+        files = [RAS_PROJECT_FILE, GEOM_FILE, PLAN1_FILE, FLOW1_FILE, RESULT1_FILE]
+        return process, payload, files
 
     @check_process
     def test4_run_incremental_normal_depth(self):
@@ -86,7 +107,8 @@ class TestApi(unittest.TestCase):
             "show_ras": False,
         }
         process = "run_incremental_normal_depth"
-        return process, payload
+        files = [PLAN2_FILE, FLOW2_FILE, RESULT2_FILE]
+        return process, payload, files
 
     @check_process
     def test5_run_known_wse(self):
@@ -100,10 +122,12 @@ class TestApi(unittest.TestCase):
             "show_ras": False,
         }
         process = "run_known_wse"
-        return process, payload
+        files = [PLAN3_FILE, FLOW3_FILE, RESULT3_FILE]
+        return process, payload, files
 
     @check_process
     def test6_create_fim_lib(self):
         payload = {"submodel_directory": f"{SUBMODELS_BASE_DIRECTORY}\\{REACH_ID}", "plans": ["nd", "kwse"]}
         process = "create_fim_lib"
-        return process, payload
+        files = [FIM_LIB_DB, DEPTH_GRIDS_ND, DEPTH_GRIDS_KWSE]
+        return process, payload, files
