46995a8e69debc8d3beb0d1aad75dc63e2a517e3
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-07-16 12:43:18 -0400

    Create central rating_curves table

---
+++ production/create_libraray_db.py
@@ -0,0 +1,54 @@
+import sqlite3
+
+# Creates the file if it doesn't exist
+connection = sqlite3.connect(r"D:\Users\abdul.siddiqui\workbench\projects\production\library.sqlite")
+cursor = connection.cursor()
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS conflation (
+        reach_id INTEGER PRIMARY KEY,
+        nwm_to_id INTEGER,
+        conflation_to_id INTEGER
+    )
+    """
+)
+
+# Indexes will speed up where downstream = queries
+cursor.execute(
+    """
+    CREATE INDEX IF NOT EXISTS conflation_nwm_to_id_idx ON conflation (nwm_to_id)
+    """
+)
+cursor.execute(
+    """
+    CREATE INDEX IF NOT EXISTS conflation_conflation_to_id_idx ON conflation (conflation_to_id)
+    """
+)
+
+cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS rating_curves (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        reach_id INTEGER,
+        us_flow REAL,
+        us_depth REAL,
+        us_wse Real,
+        ds_depth REAL,
+        ds_wse REAL,
+        boundary_condition TEXT CHECK(boundary_condition IN ('nd','kwse')) NOT NULL,
+        ripple_version TEXT,
+        UNIQUE(reach_id, us_flow, ds_wse, boundary_condition)
+    )
+    """
+)
+
+cursor.execute(
+    """
+    CREATE INDEX IF NOT EXISTS rating_curves_reach_id ON rating_curves (reach_id)
+    """
+)
+
+
+connection.commit()
+connection.close()
---
+++ production/load_rating_curves.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+
+# Paths
+library_db_path = r"D:\Users\abdul.siddiqui\workbench\projects\production\library.sqlite"
+library_dir = r"D:\Users\abdul.siddiqui\workbench\projects\production\library"
+
+
+def process_reach_db(submodel, reach_db_path, library_conn):
+    """Process a reach_id.db file and insert rating curves into the library database."""
+    reach_conn = sqlite3.connect(reach_db_path)
+    reach_cursor = reach_conn.cursor()
+    reach_cursor.execute(
+        "SELECT reach_id, us_flow, us_depth, us_wse, ds_depth, ds_wse, boundary_condition, ripple_version FROM rating_curves"
+    )
+    rows = reach_cursor.fetchall()
+
+    cursor = library_conn.cursor()
+    cursor.executemany(
+        """
+        INSERT INTO rating_curves (
+            reach_id, us_flow, us_depth, us_wse, ds_depth, ds_wse, ripple_version
+        )
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """,
+        rows,
+    )
+    library_conn.commit()
+
+    reach_conn.close()
+
+
+library_conn = sqlite3.connect(library_db_path)
+
+for submodel in os.listdir(library_dir):
+    sub_db_path = os.path.join(
+        library_dir,
+        submodel,
+        f"{submodel}.db",
+    )
+    process_reach_db(submodel, sub_db_path, library_conn)
+
+library_conn.close()
