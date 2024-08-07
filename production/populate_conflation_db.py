a89f7b4b7368ec65293b8e52d852f70361436d1e
ar-siddiqui <abdulraheemsiddiqui@gmail.com>
2024-08-07 14:47:10 -0400

    Use duckDB to populate conflation table

--- production/populate_conflation_db.py
+++ production/populate_conflation_db.py
@@ -1,34 +1,36 @@
 """
 This script reads data from a NWM Flowlines Parquet file and inserts it into an Conflation SQLite database.
-
-PyArrow is used to read the Parquet file due to its efficiency in handling columnar data formats and its ability to work well with large datasets.
-The script avoids using Pandas to reduce memory overhead and improve performance. Instead, it directly handles the Arrow Table for data extraction and insertion.
+DuckDB is used to read the Parquet file due to its efficiency in handling columnar data formats and its ability to work well with large datasets.
 The data is inserted into the SQLite database in batches using the `executemany` method to improve insertion performance and reduce the number of database transactions.
-
 """
 
 import sqlite3
 
-import pyarrow.parquet as pq
+import duckdb
 
 
-def read_parquet_to_table(parquet_file):
-    table = pq.read_table(parquet_file)
+def read_parquet_with_duckdb(parquet_file):
+
+    con = duckdb.connect()
+    query = f"SELECT id AS reach_id, to_id AS nwm_to_id FROM read_parquet('{parquet_file}')"
+    table = con.execute(query).fetchall()
+    con.close()
+
     return table
 
 
 def insert_table_to_sqlite(table, sqlite_db):
+
     conn = sqlite3.connect(sqlite_db)
     cursor = conn.cursor()
 
-    rows = [(table.column("id")[i].as_py(), table.column("to_id")[i].as_py()) for i in range(table.num_rows)]
-
+    # Prepare rows for insertion
     cursor.executemany(
         """
-    INSERT INTO conflation (reach_id, nwm_to_id)
-    VALUES (?, ?)
-    """,
-        rows,
+        INSERT INTO conflation (reach_id, nwm_to_id)
+        VALUES (?, ?)
+        """,
+        table,
     )
 
     conn.commit()
@@ -36,7 +38,7 @@ def insert_table_to_sqlite(table, sqlite_db):
 
 
 parquet_file = "D:/Users/abdul.siddiqui/workbench/projects/ripple_1d_outputs/nwm_flowlines.parquet"
-sqlite_db = "D:/Users/abdul.siddiqui/workbench/projects/production/conflation.sqlite"
+sqlite_db = "D:/Users/abdul.siddiqui/workbench/projects/test_production/library.sqlite"
 
-table = read_parquet_to_table(parquet_file)
+table = read_parquet_with_duckdb(parquet_file)
 insert_table_to_sqlite(table, sqlite_db)
