68a76e6c0284ef124eec416f95296177daba1c8c
Matt Deshotel <mdeshotel@dewberry.com>
2024-08-07 07:38:38 -0500

    read from crs table but filter by processing table

--- production/step_1_extract_geometry.py
+++ production/step_1_extract_geometry.py
@@ -36,32 +36,43 @@ def process_one_geom(
 def main(crs_table_name: str, processing_table: str, mip_group: str, bucket: str = None):
     """Read from database a list of ras files to convert to geopackage."""
     db = PGFim()
+    processing_data = db.read_cases(
+        processing_table,
+        ["s3_key"],
+        mip_group,
+        "AND gpkg_complete=false AND gpkg_exc='expected 1 result, no results found'",
+    )
+    processing_data = [i[0] for i in processing_data]
 
-    data = db.read_cases(crs_table_name, ["mip_case", "s3_key", "crs"], mip_group)
+    crs_data = db.read_cases(crs_table_name, ["s3_key", "crs"], mip_group)
 
-    for i, (mip_case, key, crs) in enumerate(data):
-        key = key.replace(f"s3://{bucket}/", "")
+    for i, (key, crs) in enumerate(crs_data):
+        if key.replace(f"s3://{bucket}/", "").replace("cases", "cases_s1") in processing_data:
+            key = key.replace(f"s3://{bucket}/", "").replace("cases", "cases_s1")
+            mip_case = key.split("cases_s1/")[1].split("/")[0]
 
-        logging.info(f"Working on ({i+1}/{len(data)} | {round(100*(i+1)/len(data),1)}% | key: {key}")
-        try:
-            _ = process_one_geom(key, crs, bucket)
-            db.update_case_status(processing_table, mip_group, mip_case, key, True, None, None, "gpkg")
+            logging.info(
+                f"Working on ({i+1}/{len(crs_data)} | {round(100*(i+1)/len(crs_data),1)}% | key: {key} | mip case: {mip_case}"
+            )
+            try:
+                _ = process_one_geom(key, crs, bucket)
+                db.update_case_status(processing_table, mip_group, mip_case, key, True, None, None, "gpkg")
 
-        except Exception as e:
-            exc = str(e)
-            tb = str(traceback.format_exc())
-            logging.error(exc)
-            logging.error(tb)
-            db.update_case_status(processing_table, mip_group, mip_case, key, False, exc, tb, "gpkg")
+            except Exception as e:
+                exc = str(e)
+                tb = str(traceback.format_exc())
+                logging.error(exc)
+                logging.error(tb)
+                db.update_case_status(processing_table, mip_group, mip_case, key, False, exc, tb, "gpkg")
 
 
 if __name__ == "__main__":
     configure_logging(level=logging.INFO, logfile="extract_geometry.log")
     load_dotenv(find_dotenv())
 
-    crs_table_name = "inferred_crs"
+    crs_table_name = "inferred_crs_v2"
     bucket = "fim"
-    mip_group = "tx_ble"
-    processing_table = "processing_v2"
+    mip_group = "b"
+    processing_table = "processing_s1"
 
     main(crs_table_name, processing_table, mip_group, bucket)
