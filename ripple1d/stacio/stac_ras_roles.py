74c5b59f16d209a9a25ed48ab6729694a277dc72
sclaw <klawson@dewberry.com>
2024-09-13 12:00:26 -0400

    retain asset titles as cleaned file name instead of informational title

--- ripple1d/stacio/stac_ras_roles.py
+++ ripple1d/stacio/stac_ras_roles.py
@@ -49,21 +49,15 @@ def add_extra_fields(asset_key: str, base_asset: dict, bucket: str = None) -> di
     if "steady-flow-file" in base_asset["roles"]:
         asset_string = get_asset_string(asset_key, client, bucket)
         flow = RasFlowText.from_str(asset_string, " .f01")
-        base_asset["extra_fields"]["title"] = flow.title
         base_asset["extra_fields"]["number_of_profiles"] = flow.n_profiles
         base_asset["extra_fields"]["profile_names"] = flow.profile_names
     if "geometry-file" in base_asset["roles"]:
         asset_string = get_asset_string(asset_key, client, bucket)
         geom = RasGeomText.from_str(asset_string, "EPSG:4326", " .g01")  # Dummy CRS
-        base_asset["extra_fields"]["title"] = geom.title
         base_asset["extra_fields"]["number_of_rivers"] = geom.n_rivers
         base_asset["extra_fields"]["number_of_reaches"] = geom.n_reaches
         base_asset["extra_fields"]["number_of_cross_sections"] = geom.n_cross_sections
         base_asset["extra_fields"]["number_of_junctions"] = geom.n_junctions
-    if "plan-file" in base_asset["roles"]:
-        asset_string = get_asset_string(asset_key, client, bucket)
-        plan = RasPlanText.from_str(asset_string, "EPSG:4326", " .p01")  # Dummy CRS
-        base_asset["extra_fields"]["title"] = plan.title
 
     return base_asset
 
