706f011d3d90abefbe9f023ff0bb8d041b4f556e
Matt Deshotel <mdeshotel@dewberry.com>
2024-09-13 11:56:14 -0500

    delete raw ras grids Closes#151

--- ripple1d/ops/fim_lib.py
+++ ripple1d/ops/fim_lib.py
@@ -3,6 +3,7 @@
 import json
 import logging
 import os
+import shutil
 from datetime import datetime
 from pathlib import Path, PurePosixPath
 
@@ -127,10 +128,11 @@ def post_process_depth_grids(
 def create_fim_lib(
     submodel_directory: str,
     plans: list,
+    cleanup: bool,
     ras_version: str = "631",
     table_name: str = "rating_curves",
-    tiled=False,
-    overviews=False,
+    tiled: bool = False,
+    overviews: bool = False,
     resolution: float = 3,
     resolution_units: str = "Meters",
 ):
@@ -168,6 +170,8 @@ def create_fim_lib(
                 nwm_rm.fim_results_database,
                 table_name,
             )
+            if cleanup:
+                shutil.rmtree(os.path.join(rm.ras_project._ras_dir, f"{nwm_rm.model_name}_kwse"))
         if f"nd" in plan:
             zero_depth_to_sqlite(
                 rm,
@@ -177,6 +181,8 @@ def create_fim_lib(
                 nwm_rm.fim_results_database,
                 table_name,
             )
+            if cleanup:
+                shutil.rmtree(os.path.join(rm.ras_project._ras_dir, f"{nwm_rm.model_name}_nd"))
 
     return {"fim_results_directory": nwm_rm.fim_results_directory, "fim_results_database": nwm_rm.fim_results_database}
 
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -161,7 +161,7 @@ class TestApi(unittest.TestCase):
 
     @check_process
     def test_g_create_fim_lib(self):
-        payload = {"submodel_directory": self.SUBMODELS_DIRECTORY, "plans": ["nd", "kwse"]}
+        payload = {"submodel_directory": self.SUBMODELS_DIRECTORY, "plans": ["nd", "kwse"], "cleanup": True}
         process = "create_fim_lib"
         files = [self.FIM_LIB_DB, self.DEPTH_GRIDS_ND, self.DEPTH_GRIDS_KWSE]
         return process, payload, files
