fe6511da18b656787722188357e42ddfe4056c8d
Matt Deshotel <mdeshotel@dewberry.com>
2024-09-18 09:17:50 -0500

    check if reach is eclipsed Closes #190

--- ripple1d/ops/subset_gpkg.py
+++ ripple1d/ops/subset_gpkg.py
@@ -479,7 +479,7 @@ def extract_submodel(source_model_directory: str, submodel_directory: str, nwm_i
         raise FileNotFoundError(f"cannot find conflation file {rsd.conflation_file}, please ensure file exists")
 
     ripple1d_parameters = rsd.nwm_conflation_parameters(str(nwm_id))
-    if ripple1d_parameters["us_xs"]["xs_id"] == "-9999":
+    if ripple1d_parameters["eclipsed"]:
         ripple1d_parameters["messages"] = f"skipping {nwm_id}; no cross sections conflated."
         logging.warning(ripple1d_parameters["messages"])
 
