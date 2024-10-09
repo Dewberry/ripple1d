7543b0063a5ae0727b477965432b230221813ba8
Seth Lawler <slawler@dewberry.com>
2024-10-09 09:30:56 -0400

    add custom logger to jobs (subprocesses)

--- ripple1d/ops/endpoints.py
+++ ripple1d/ops/endpoints.py
@@ -1,8 +1,10 @@
 import json
+import logging
 import subprocess
 import sys
 from typing import Callable
 
+from ripple1d.consts import SUPPRESS_LOGS
 from ripple1d.ops.fim_lib import create_fim_lib, fim_lib_stac, nwm_reach_model_stac
 from ripple1d.ops.metrics import compute_conflation_metrics
 from ripple1d.ops.ras_conflate import conflate_model
@@ -14,6 +16,7 @@ from ripple1d.ops.ras_run import (
 from ripple1d.ops.ras_terrain import create_ras_terrain
 from ripple1d.ops.subset_gpkg import extract_submodel
 from ripple1d.ras_to_gpkg import gpkg_from_ras
+from ripple1d.ripple1d_logger import RippleLogFormatter  # , initialize_process_logger
 
 func_lookup = {
     "extract_submodel": extract_submodel,
@@ -35,7 +38,23 @@ def execute_endpoint(func: str, args: dict):
     return func_lookup[func](**json.loads(args))
 
 
+def initialize_process_logger(level, verbose: bool = False):
+    """Configure logging for ripple1d."""
+    datefmt = "%Y-%m-%dT%H:%M:%SZ"
+
+    for module in SUPPRESS_LOGS:
+        logging.getLogger(module).setLevel(logging.WARNING)
+
+    handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(
+        level=level,
+        handlers=[handler],
+        format="""{"time": "%(asctime)s" , "level": "%(levelname)s", "msg": "%(message)s"}""",
+    )
+
+
 if __name__ == "__main__":
+    initialize_process_logger(logging.INFO)
     try:
         r = execute_endpoint(sys.argv[1], sys.argv[2])
         print((json.dumps({"results": r})))
