799fd482f7418878ec59ccd1be4f92c10b766f30
Matt Deshotel <mdeshotel@dewberry.com>
2024-10-11 12:01:53 -0500

    add doc strings

--- ripple1d/ops/subset_gpkg.py
+++ ripple1d/ops/subset_gpkg.py
@@ -36,24 +36,28 @@ class RippleGeopackageSubsetter:
         self._ripple_xs_concave_hull = None
 
     def set_nwm_id(self, nwm_id: str):
+        """Set the network ID."""
         self.nwm_id = nwm_id
         self._subset_gdf = None
         self._ripple_xs_concave_hull = None
 
     @property
     def ripple_us_xs(self):
+        """The most upstream cross section for the ripple model."""
         return self.ripple_xs.loc[
             self.ripple_xs["river_station"] == self.ripple_xs["river_station"].max(), "geometry"
         ].iloc[0]
 
     @property
     def ripple_ds_xs(self):
+        """The most downstream cross section for the ripple model."""
         return self.ripple_xs.loc[
             self.ripple_xs["river_station"] == self.ripple_xs["river_station"].min(), "geometry"
         ].iloc[0]
 
     @property
     def split_source_hull(self):
+        """Split the concave hull of the source model using the upstream and downstream cross sections of the submodel."""
         geoms = split(self.source_hulls.geometry.iloc[0], self.ripple_us_xs).geoms
         hulls = []
         for geom in geoms:
@@ -70,6 +74,7 @@ class RippleGeopackageSubsetter:
 
     @property
     def ripple_xs_concave_hull(self):
+        """Get the concave hul of the cross sections."""
         if self._ripple_xs_concave_hull is None:
             try:
                 hulls = self.split_source_hull
