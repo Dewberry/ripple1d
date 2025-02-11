5b16cb5b2498b383aefdc4ea5a920bf6d3ce8810
sclaw <klawson@dewberry.com>
2025-02-11 12:30:39 -0500

    update plotting for haribo twin snake section lines

--- tests/conflation_tests/conflation_test.py
+++ tests/conflation_tests/conflation_test.py
@@ -7,15 +7,15 @@ from tests.conflation_tests.plotting import plot_conflation
 def run_scenario(ras_dir_name: str):
     """Run a specific test case."""
     pm = PathManager(ras_dir_name)
-    conflate_model(pm.ras_dir, pm.ras_model_name, pm.nwm_dict)
+    # conflate_model(pm.ras_dir, pm.ras_model_name, pm.nwm_dict)
     plot_conflation(pm.ras_path, pm.nwm_path, pm.conflation_file)
     ConflationFile(pm.conflation_file) == ConflationFile(pm.rubric_file)  # Validation
 
 
 def run_all():
     """Run all conflation tests."""
-    # for test in ["test_a", "test_b", "test_c", "test_d"]:
-    for test in ["test_f"]:
+    for test in ["test_a", "test_b", "test_c", "test_d"]:
+        # for test in ["test_f"]:
         run_scenario(test)
 
 
--- tests/conflation_tests/plotting.py
+++ tests/conflation_tests/plotting.py
@@ -1,4 +1,7 @@
 import json
+from collections import defaultdict
+from copy import copy
+from operator import sub
 from typing import OrderedDict
 
 import contextily as ctx
@@ -6,11 +9,16 @@ import fiona
 import geopandas as gpd
 import matplotlib.patheffects as path_effects
 import matplotlib.pyplot as plt
+import numpy as np
 import shapely
-from matplotlib import axes
+from matplotlib import axes, figure
 from matplotlib.colors import ListedColormap
+from pyproj import CRS
+from shapely import LineString, Point, distance, get_parts
+from shapely.ops import split
 
 from ripple1d.ops.subset_gpkg import RippleGeopackageSubsetter
+from tests.conflation_tests.consts import JUNCTION_RAS_DATA, RIVER_RAS_DATA, XS_RAS_DATA
 
 custom_colors = [
     "#01FF70",
@@ -53,9 +61,9 @@ def offset_xs(gdf: gpd.GeoDataFrame, pts: float, ax: axes, rch_dict: dict) -> gp
     return gdf
 
 
-def plot_conflation(ras_gpkg: str, nwm_path: str, conflation_path: str) -> str:
-    """Create summary png for conflation results."""
-    # Load data
+def load_data(
+    ras_gpkg: str, nwm_path: str, conflation_path: str
+) -> tuple[dict[gpd.GeoDataFrame], CRS, gpd.GeoDataFrame, gpd.GeoDataFrame, dict]:
     gdfs = {l: gpd.read_file(ras_gpkg, layer=l) for l in fiona.listlayers(ras_gpkg)}
     crs = gdfs[next(iter(gdfs))].crs
     nwm = gpd.read_parquet(nwm_path).to_crs(crs)
@@ -63,25 +71,90 @@ def plot_conflation(ras_gpkg: str, nwm_path: str, conflation_path: str) -> str:
     nwm_endpts["geometry"] = shapely.get_point(nwm.geometry, 0)
     with open(conflation_path) as f:
         conflation = json.load(f)
+    return gdfs, crs, nwm, nwm_endpts, conflation
 
-    # Plot
-    rows = 3
-    w = 6
-    fig, axs = plt.subplots(nrows=rows, sharex=True, sharey=True)
 
+def add_default_ras_data(gdfs: dict[gpd.GeoDataFrame], axs: list[axes.Axes]):
     for layer, kwargs in LAYER_COLORS.items():
         if layer in gdfs.keys():
             gdfs[layer].plot(ax=axs[0], **kwargs)
     gdfs["River"].plot(ax=axs[2], color="k", lw=1, label="HEC-RAS River")
 
+
+def add_default_nwm_data(nwm, nwm_endpts, axs):
     for ind, r in enumerate(nwm["ID"].astype(str).values[::-1]):
         color = custom_colors[ind]
         sub_nwm = nwm[nwm["ID"] == int(r)]
         sub_nwm_endpts = nwm_endpts[nwm_endpts["ID"] == int(r)]
         sub_nwm.plot(ax=axs[1], **LAYER_COLORS["nwm"], label=f"{r}", color=color)
         sub_nwm_endpts.plot(ax=axs[1], **LAYER_COLORS["nwm"], color=color)
-        if r not in conflation["reaches"]:
+
+
+def get_overlaps(conflation: dict) -> dict:
+    overlaps = defaultdict(list)
+    for r in conflation["reaches"]:
+        if conflation["reaches"][r]["eclipsed"]:
             continue
+        for xs in ["us_xs", "ds_xs"]:
+            xs_tuple = tuple([str(conflation["reaches"][r][xs][i]) for i in ["river", "reach", "xs_id"]])
+            overlaps[xs_tuple].append(r)
+    overlaps = {k: tuple(v) for k, v in overlaps.items() if len(v) > 1}
+    return overlaps
+
+
+def divide_section(line, splits):
+    subs = []
+    dists = np.linspace(0, line.length, splits + 1, endpoint=True)[1:]
+    pt1 = Point(line.coords[0])
+    d = 0
+    segment = 0
+    cur_coords = [pt1]
+    ind = 1
+    while ind <= len(line.coords):
+        pt2 = Point(line.coords[ind])
+        inc_dist = distance(pt1, pt2)
+        new_d = d + inc_dist
+        if new_d - dists[segment] > 1:
+            new_pt = Point(line.interpolate(dists[segment]))
+            cur_coords.append(new_pt)
+            sub = LineString(cur_coords)
+            subs.append(sub)
+            cur_coords = [new_pt]
+            pt1 = new_pt
+            d = dists[segment]
+            segment += 1
+        elif abs(new_d - dists[segment]) < 1:
+            cur_coords.append(pt2)
+            sub = LineString(cur_coords)
+            subs.append(sub)
+            cur_coords = [pt2]
+            pt1 = pt2
+            d = dists[segment]
+            segment += 1
+            ind += 2
+        else:
+            cur_coords.append(pt2)
+            pt1 = pt2
+            d += inc_dist
+            ind += 1
+    return subs
+
+
+def clip_xs(subset, overlaps, r):
+    for xs_id in subset["river_reach_rs"].values:
+        refactor_id = tuple(xs_id.split("_"))
+        if refactor_id in list(overlaps.keys()):
+            splits = len(overlaps[refactor_id])
+            position = overlaps[refactor_id].index(r)
+            geom = subset.loc[subset["river_reach_rs"] == xs_id, "geometry"].values[0]
+            subset.loc[subset["river_reach_rs"] == xs_id, "geometry"] = divide_section(geom, splits)[position]
+    return subset
+
+
+def add_conflated_data(conflation, ras_gpkg, conflation_path, axs, nwm, nwm_endpts):
+    overlaps = get_overlaps(conflation)
+    for ind, r in enumerate(conflation["reaches"]):
+        color = custom_colors[ind]
         if conflation["reaches"][r]["eclipsed"]:
             ls = "dashed"
             label = f"{r} (eclipsed)"
@@ -91,12 +164,17 @@ def plot_conflation(ras_gpkg: str, nwm_path: str, conflation_path: str) -> str:
 
             subsetter = RippleGeopackageSubsetter(ras_gpkg, conflation_path, None, r)
             subset = subsetter.subset_xs
-            subset = offset_xs(subset, 1.25, axs[2], conflation["reaches"][r])
+            subset = clip_xs(subset, overlaps, r)
             subset.plot(ax=axs[2], color="k", lw=3, zorder=2)
             subset.plot(ax=axs[2], color=color, lw=2, zorder=2)
+
+        sub_nwm = nwm[nwm["ID"] == int(r)]
+        sub_nwm_endpts = nwm_endpts[nwm_endpts["ID"] == int(r)]
         sub_nwm.plot(ax=axs[2], **LAYER_COLORS["nwm"], label=label, ls=ls, color=color, zorder=1)
         sub_nwm_endpts.plot(ax=axs[2], **LAYER_COLORS["nwm"], color=color, zorder=1)
 
+
+def format(axs: list[axes.Axes], fig: figure, conflation, crs, w):
     # Add labels
     txt = axs[0].text(0.05, 0.95, "HEC-RAS Source Model", transform=axs[0].transAxes, va="top", ha="left", fontsize=8)
     txt.set_path_effects([path_effects.withStroke(linewidth=3, foreground="white")])
@@ -123,6 +201,22 @@ def plot_conflation(ras_gpkg: str, nwm_path: str, conflation_path: str) -> str:
     ar = (height) / width
     fig.set_size_inches(w, 3 * ar * w, forward=True)
     fig.tight_layout()
+
+
+def plot_conflation(ras_gpkg: str, nwm_path: str, conflation_path: str) -> str:
+    """Create summary png for conflation results."""
+    # Load data
+    gdfs, crs, nwm, nwm_endpts, conflation = load_data(ras_gpkg, nwm_path, conflation_path)
+
+    # Plot
+    rows = 3
+    w = 6
+    fig, axs = plt.subplots(nrows=rows, sharex=True, sharey=True)
+
+    add_default_ras_data(gdfs, axs)
+    add_default_nwm_data(nwm, nwm_endpts, axs)
+    add_conflated_data(conflation, ras_gpkg, conflation_path, axs, nwm, nwm_endpts)
+    format(axs, fig, conflation, crs, w)
 
     # Save
     out_path = conflation_path.replace(".json", ".png")
