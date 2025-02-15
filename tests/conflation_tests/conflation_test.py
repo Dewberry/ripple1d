a61d713c101f6f09b1c3cc0b7c045cb781ec5d7a
Matt Deshotel <mdeshotel@dewberry.com>
2025-02-15 09:03:22 -0600

    add test n

--- tests/conflation_tests/conflation_test.py
+++ tests/conflation_tests/conflation_test.py
@@ -4,14 +4,27 @@ from ripple1d.ops.ras_conflate import conflate_model
 from tests.conflation_tests.classes import ConflationFile, PathManager
 from tests.conflation_tests.plotting import plot_conflation
 
-tests = ["test_a", "test_b", "test_c", "test_d", "test_e", "test_f", "test_g", "test_h", "test_i", "test_j", "test_k"]
+tests = [
+    "test_a",
+    "test_b",
+    "test_c",
+    "test_d",
+    "test_e",
+    "test_f",
+    "test_g",
+    "test_h",
+    "test_i",
+    "test_j",
+    "test_k",
+    "test_n",
+]
 
 
 @pytest.mark.parametrize("ras_dir_name", tests)
 def test_conflation(ras_dir_name: str, generate_plots: bool = False):
     """Run a specific test case."""
     pm = PathManager(ras_dir_name)
-    conflate_model(pm.ras_dir, pm.ras_model_name, pm.nwm_dict)
+    conflate_model(pm.ras_dir, pm.ras_model_name, pm.nwm_dict, False)
     if generate_plots:
         plot_conflation(pm.ras_path, pm.nwm_path, pm.conflation_file)
     ConflationFile(pm.conflation_file) == ConflationFile(pm.rubric_file)  # Validation
